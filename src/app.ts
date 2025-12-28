67b713b5838933983bc7f4a9108f5731a4e963ca
asifahmed1810 <asifahmed18oct@gmail.com>
2025-12-29 00:04:31 +0600

    Added auth.ts file and update app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,20 @@
 import express from "express"
+import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app=express();
+
+app.use(cors({
+    origin:process.env.APP_URL || "http://localhost:4000 ",
+    credentials:true
+}))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
+app.use(express.json())
+app.use("/posts",postRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hello World")
@@ -8,3 +22,4 @@ app.get("/",(req,res)=>{
 
 
 export default app;
+

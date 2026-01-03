1ea0d413104c7d75f0055965cba2b7ee1165c60d
asifahmed1810 <asifahmed18oct@gmail.com>
2026-01-03 21:21:24 +0600

    Added scripts folder

---
+++ src/scripts/seedAdmin.ts
@@ -0,0 +1,39 @@
+import { email } from "better-auth/*";
+import { prisma } from "../lib/prisma";
+import { UserRole } from "../middlewares/auth";
+
+async function seedAdmin() {
+    try {
+
+        const adminData={
+            name:"Admin",
+            email:"admin@gmail.com",
+            role:UserRole.ADMIN,
+            password:"Admin1234"
+        }
+
+
+        //check user exist on db or not
+
+        const existingUser= await prisma.user.findUnique({
+            where:{
+                email:adminData.email
+            }
+        })
+
+        if(existingUser){
+            throw new Error("User already exist in DB")
+        }
+
+        const signupAdmin=await fetch("http://localhost:3000/api/auth/sign-up/email",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(adminData)
+        })
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
